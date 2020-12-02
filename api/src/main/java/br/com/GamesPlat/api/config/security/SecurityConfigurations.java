00509bb4dce3da0d3be687bd2ac73faaeabefeb7
Saulo <saulojar@gmail.com>
2020-12-02 17:12:37 -0300

    Gerando JWT

--- api/src/main/java/br/com/GamesPlat/api/config/security/SecurityConfigurations.java
+++ api/src/main/java/br/com/GamesPlat/api/config/security/SecurityConfigurations.java
@@ -1,20 +1,36 @@
 package br.com.GamesPlat.api.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private AutenticacaoService autenticacaoService;
+	
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
 	//Configurações de autenticação
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 	}
 
 	//configurações de autorização
@@ -22,9 +38,11 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
-			.antMatchers(HttpMethod.GET,"/user/*").permitAll()
+			//.antMatchers(HttpMethod.GET,"/user/*").permitAll()
+			.antMatchers(HttpMethod.POST, "/auth").permitAll()
 			.anyRequest().authenticated()
-			.and().formLogin();
+			.and().csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 	}
 	
---
+++ api/src/main/java/br/com/GamesPlat/api/config/security/TokenService.java
@@ -0,0 +1,36 @@
+package br.com.GamesPlat.api.config.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import br.com.GamesPlat.api.models.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class TokenService {
+	
+	@Value("${api.jwt.expiration}")
+	private String expiration;
+
+	@Value("${api.jwt.secret}")
+	private String secret;
+	
+	public String gerarToken(Authentication authentication) {
+		Usuario logado = (Usuario) authentication.getPrincipal();
+		Date hoje = new Date();
+		Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
+		
+		return Jwts.builder()
+				.setIssuer("API de CompJogos")
+				.setSubject(logado.getId().toString())
+				.setIssuedAt(hoje)
+				.setExpiration(dataExpiracao)
+				.signWith(SignatureAlgorithm.HS256, secret)
+				.compact();
+	}
+
+}
